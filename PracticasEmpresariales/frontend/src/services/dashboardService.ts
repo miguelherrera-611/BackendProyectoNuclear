b7bb583a0cbd8f73325e83799a20f1d012a33e2f
Miguel Herrera <maherrera_115@cue.edu.co>
2026-06-22 23:20:26 -0500

    cambio de estado a false en la exposicion del 2fa

--- PracticasEmpresariales/frontend/src/services/dashboardService.ts
+++
@@ -1,9 +0,0 @@
-import api from './api'
-import { ApiResponse, DashboardResponse } from '../types'
-
-export const dashboardService = {
-  async obtener(): Promise<DashboardResponse> {
-    const res = await api.get<ApiResponse<DashboardResponse>>('/dashboard')
-    return res.data.datos!
-  },
-}
